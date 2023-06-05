be57fe7c80db5924d57ea0a7902b7deede98b216
WaelSBoles <128249028+WaelSBoles@users.noreply.github.com>
2023-06-04 20:33:36 -0400

    Update Task2.java

--- src/test/java/Class6/Task2.java
+++ src/test/java/Class6/Task2.java
@@ -23,5 +23,7 @@ public class Task2 extends CommonMethods {
         System.out.println(fname.getText());
         WebElement lname= driver.findElement(By.xpath("//p[contains(text(),'Last Name ')]"));
         System.out.println(lname.getText());
+         Thread.sleep(2000);
+       driver.close();
     }
 }
